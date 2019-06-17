52367d16001387f79ce253e4ba70ab71858e8d60
kuznetsovin <kuznetsovin@gmail.com>
2019-06-17 13:08:56 +0300

    правка тестингового скрипта

--- scripts/testing.sh
+++ scripts/testing.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-go test github.com/kuznetsovin/galileosky-protocol/pkg/packet -cover
+go test github.com/kuznetsovin/galileosky-protocol/pkg/galileo -cover
 go test github.com/kuznetsovin/galileosky-protocol/cmd/receiver -cover
